e02df9e27a9ba667e7a23d8b203d4807436b89c7
Lee <lee@localhost.localdomain>
2018-04-11 10:03:54 +0800

    增加內联和引用

--- chapter_8/main.cpp
+++ chapter_8/main.cpp
@@ -11,15 +11,31 @@
 
 using namespace std;
 
+inline int inline_test(); //內联函数可以用在递归
+int cite_test(int & n); //修改了n值会修改传入n的变量的值
 
 int main(int agrs, char *argv[])
 {
+	//inline_test();
 
-	cout << "start\n" << endl; 
+	int a = 4;
+	cite_test(a);
+	cout << a << endl;
 
 	cin.get();
 	cin.get();
 	return 0;
 }
 
+int cite_test(int & n)
+{
+	n = n + 1; 
+	return 0;
+}
 
+inline int inline_test()
+{
+	cout << "o\n";
+	inline_test();
+	return 0;
+}
